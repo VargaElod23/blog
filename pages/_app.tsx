1856341adfdcdb8aef33ea7d59a7d3ae63b45b0a
VargaElod23 <elod.varga@taraxa.io>
2023-06-21 14:23:05 +0300

    implements mq hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import { useRouter } from "next/router";
+import React from "react";
+import Loader from "../components/loader";
+import useLoader from "../hooks/useLoader";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { isLoading, setIsLoading } = useLoader();
+
+  React.useEffect(() => {
+    const handleStart = () => {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+
+      // Clear the timeout if the component unmounts
+      return () => clearTimeout(timer);
+    };
+
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      // router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+  return (
+    <>
+      {isLoading && <Loader />}
+      <Component {...pageProps} />
+    </>
+  );
 }
