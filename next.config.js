ebe0d33f87642d19f606a2b8bb2442c9858cba79
VargaElod23 <elod.varga@taraxa.io>
2023-07-10 11:31:27 +0300

    adds 1h cache to github summary endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/gihub/summary",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=3600, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
