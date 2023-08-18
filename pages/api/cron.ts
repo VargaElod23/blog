b947590f74ebfad7e1d56b1c223eca9e031ed619
VargaElod23 <elod.varga@taraxa.io>
2023-08-18 13:25:14 +0300

    goes back to one call per validator

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import ApyOracle from "./ApyOracle.json";
 import Multicall from "./Multicall.json";
-import { ethers } from "ethers";
+import { JsonRpcProvider, ethers } from "ethers";
 
 interface Network {
   chainName: string;
@@ -83,29 +83,33 @@ interface ValidatorData {
 }
 
 interface ValidatorYield {
+  apy: number;
   fromBlock: number;
   toBlock: number;
-  yield: number;
 }
 
 interface ValidatorDetails {
-  address: string;
+  account: string;
   pbftCount: number;
   rank: number;
-  yield: ValidatorYield;
+  apy: number;
+  fromBlock: number;
+  toBlock: number;
 }
 const chainId = process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 842;
 
 const getValidatorYield = async (
   validatorAddress: string
 ): Promise<ValidatorYield> => {
-  const validatorYield = await axios.get(
-    `${networks[chainId].indexerUrl}/address/${validatorAddress}/yield`
-  );
+  const url = `${networks[chainId].indexerUrl}/address/${validatorAddress}/yield`;
+  const validatorYield = await axios.get(url);
   if (validatorYield.status === 200) {
     const { data } = validatorYield;
-
-    return data;
+    return {
+      apy: parseInt((parseFloat(data.yield) * 10000).toString()),
+      fromBlock: parseInt(data.fromBlock),
+      toBlock: parseInt(data.toBlock),
+    };
   }
 };
 
@@ -121,7 +125,7 @@ const fetchValidatorStatsForWeek = async (): Promise<ValidatorData[]> => {
       if (validators.status === 200) {
         const { data } = validators;
         const { hasNext, data: validatorData } = data;
-        allValidators.push(validatorData);
+        allValidators.push(...validatorData);
         hasNextPage = hasNext;
         start += 100;
       }
@@ -134,32 +138,92 @@ const fetchValidatorStatsForWeek = async (): Promise<ValidatorData[]> => {
   return allValidators;
 };
 
+const fetchValidatorFinalData = async (
+  dataFeedContract: ethers.Contract,
+  multicallContract: ethers.Contract,
+  oracleAddress: string,
+  provider: ethers.JsonRpcProvider
+): Promise<void> => {
+  const validatorData = await fetchValidatorStatsForWeek();
+  console.log("=============== FETCHING VALIDATORS DONE ===============");
+  const updateCalls: Call[] = [];
+  if (!validatorData.length) throw new Error("No validator data found");
+  for (const validator of validatorData) {
+    console.log(
+      `=============== FETCHING STATS FOR ${validator.address} ===============`
+    );
+    const validatorYield = await getValidatorYield(validator.address);
+    const validatorDetails: ValidatorDetails = {
+      account: validator.address,
+      pbftCount: validator.pbftCount,
+      rank: validator.rank,
+      ...validatorYield,
+    };
+    const values = [validatorDetails.account, validatorDetails];
+
+    console.log(values);
+
+    const updateCall = await dataFeedContract.updateNodeData(
+      validatorDetails.account,
+      validatorDetails
+    );
+    console.log(updateCall);
+    const tx = await updateCall.wait();
+    console.log(tx);
+
+    console.log("=============== UPDATING VALIDATOR STATS ===============");
+    // const updateCallData = dataFeedContract.interface.encodeFunctionData(
+    //   "updateNodeData",
+    //   values
+    // );
+
+    // updateCalls.push({ target: oracleAddress, callData: updateCallData });
+  }
+  //   console.log("=============== FETCHING VALIDATORS DONE ===============");
+  //   console.log("=============== UPDATING VALIDATOR STATS ===============");
+  //   const tx = await multicallContract.aggregate([updateCalls[0]]);
+  //   console.log(tx);
+  //   if (!tx) throw new Error("No tx found");
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const chainId = process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 842;
+  if (!chainId) {
+    res.status(500).end("No chain id found");
+    return;
+  }
+  const provider = new JsonRpcProvider(networks[chainId].rpcUrl);
+
   const privkey = process.env.SIGNER_PRIV_KEY;
   if (!privkey) {
     res.status(500).end("No private key found");
     return;
   }
-  const dataFeedWallet = new ethers.Wallet(privkey);
+  const dataFeedWallet = new ethers.Wallet(privkey, provider);
   const oracleAddress = process.env.ORACLE_TESTNET_ADDRESS;
-  if (!privkey) {
+  if (!oracleAddress) {
     res.status(500).end("No oracle contract address found");
     return;
   }
-  const dataFeedContract = new ethers.Contract(oracleAddress, ApyOracle.abi);
+  const dataFeedContract = new ethers.Contract(
+    oracleAddress,
+    ApyOracle.abi,
+    dataFeedWallet
+  );
 
-  const multicallAddress = process.env.MULTICALL_TESTNET_ADDRESS;
-  if (!privkey) {
+  const multicallAddress = process.env.MULTICALL_MAINNET_ADDRESS;
+  if (!multicallAddress) {
     res.status(500).end("No multicall contract address found");
     return;
   }
 
   const multicallContract = new ethers.Contract(
     multicallAddress,
-    Multicall.abi
+    Multicall.abi,
+    dataFeedWallet
   );
 
   console.log("dataFeedContract initialized at: ", oracleAddress);
@@ -167,32 +231,15 @@ export default async function handler(
 
   console.log("=============== FETCHING VALIDATORS ===============");
 
-  const validatorData = await fetchValidatorStatsForWeek();
-  console.log("=============== FETCHING VALIDATORS DONE ===============");
-  const updateCalls: Call[] = [];
-  for (const validator of validatorData) {
-    const validatorYield = await getValidatorYield(validator.address);
-    const validatorDetails: ValidatorDetails = {
-      address: validator.address,
-      pbftCount: validator.pbftCount,
-      rank: validator.rank,
-      yield: validatorYield,
-    };
-
-    const updateCallData = dataFeedContract.interface.encodeFunctionData(
-      "updateNodeData",
-      [validatorDetails.address, validatorDetails]
+  try {
+    await fetchValidatorFinalData(
+      dataFeedContract,
+      multicallContract,
+      oracleAddress,
+      provider
     );
-
-    updateCalls.push({ target: oracleAddress, callData: updateCallData });
+    res.status(200).json({ message: `Updated oracle state at ${Date.now()}` });
+  } catch (error) {
+    res.status(500).end(error.message);
   }
-
-  console.log("=============== FETCHING VALIDATORS DONE ===============");
-  console.log("=============== UPDATING VALIDATOR STATS ===============");
-  const tx = await multicallContract.aggregate(updateCalls, {
-    from: dataFeedWallet.address,
-  });
-  if (!tx) throw new Error("No tx found");
-
-  res.status(200).json({ message: `Updated oracle state at ${Date.now()}` });
 }
